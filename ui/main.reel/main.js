8706dcb14714cce4de39a6f850caaf221a3fa089
JLLindsey <noreply>
2015-11-17 18:26:06 +0000

    Update components

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -13,12 +13,30 @@ exports.Main = Component.specialize(/** @lends Main# */ {
             this.super();
         }
     },
+    enterDocument: {
+        value: function (firstTime) {
+            if (firstTime) {
+                this.templateObjects.logoutCondition.condition = false;
+            }
+        }
+    },
+    logoutActionHandler: {
+        value: function (event) {
+            this.dispatchEventNamed("loginEvent", true, false, {successfule;false});
+            event.stopPropogation();
+        }
+    },
     handleLogin: {
         value: function (event) {
          this.loggedIn = event.detail.successful;
             if (this.loggedIn) {
+             this.templateObjects.logoutCondition.condition = true;
              this.templateObjects.substitution.switchValue = "default";   
+            } else {
+                this.templateObjects.logoutCondition.condition = false;
+                this.templateObjects.substitution.switchValue = "login"; 
             }
+                
         }
     }
 });
