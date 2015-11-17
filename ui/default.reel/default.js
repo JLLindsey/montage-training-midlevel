a66fddac9c890a45c3ad35105f9f3a7050af0884
JLLindsey <noreply>
2015-11-17 15:37:11 +0000

    Add component default

---
+++ ui/default.reel/default.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/default.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Default
+ * @extends Component
+ */
+exports.Default = Component.specialize(/** @lends Default# */ {
+    constructor: {
+        value: function Default() {
+            this.super();
+        }
+    }
+});
